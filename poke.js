74340ba3eacdeaaaeb718729c79a623fe780b589
Redbear101o <qiudaniel35@gmail.com>
2024-12-11 17:30:51 -0800

    added pokemon button and background

---
+++ poke.js
@@ -0,0 +1,24 @@
+const display = document.getElementById('display')
+
+async function getPokemon(){
+    const URL = "https://elianrenteria.me/api/pokemon"
+    const response = await fetch(URL);
+    const json = await response.json()
+    console.log(json)
+    display.src = '';
+    return json
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
