f11c160f0233609d6394478b35108df6c5d6eefa
Redbear101o <qiudaniel35@gmail.com>
2025-01-18 20:43:59 -0800

    stuff added

--- snake.js
+++ snake.js
@@ -2,9 +2,45 @@ import kaboom from "https://unpkg.com/kaboom@3000.0.1/dist/kaboom.mjs";
 
 kaboom({width: 500, height: 500 });
 
-screen("title", ()=>{
+scene("title", ()=>{
     add([
-        text("snake", 32), 
-        pos(0, 0)
+        color(7, 231, 255),
+        rect(800, 800),
     ])
+    add([
+        text("Snake"),
+        pos(250, 250),
+        color(0, 0, 0),
+        anchor("center")
+    ])
+    add([
+        text("Press Space to Start!", 32), 
+        pos(250, 300),
+        color(0, 0, 0),
+        anchor("center")
+    ])
+
+    onKeyDown("space", () =>{
+        go("game");
+    })
+    
 } )
+
+scene("game", ()=>{
+    add([
+        color(237, 191, 255),
+        rect(800, 800),
+    ])
+
+    let snake = []; 
+
+    snake.push[add([
+        rect(30, 30),
+        pos(250, 250),
+        color(0, 0, 0),
+        anchor("center")
+    ])]
+})
+
+
+go("title")
